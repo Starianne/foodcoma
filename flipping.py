7a332dce1ef176f31907e2f61ccf0fad1e8595c3
Annelies <118388665+Starianne@users.noreply.github.com>
2026-04-25 21:35:46 -0500

    Hopefully fixed flipping and added a started stacking

--- flipping.py
+++ flipping.py
@@ -1,12 +1,16 @@
 import pygame
 from random import randint
+from stacking import Stacking
 
 class Flipping:
     def __init__(self, screen, clock):
         self.screen = screen
-        self.screen_width, self.screen_height = self.screen
         self.clock = clock
         self.dt = 0
+        self.running = True
+        self.completed = False
+        self.screen_width = self.screen.get_width()
+        self.screen_height = self.screen.get_height()
 
         self.flip_meter_surface1 = pygame.image.load('assets/imgs/FOODCOMA.png').convert_alpha()  
         self.flip_meter_rect1 = self.flip_meter_surface1.get_rect(center = (4*self.screen_width/5, self.screen_height/10))
@@ -17,26 +21,40 @@ class Flipping:
         self.flip_meter_surface4 = pygame.image.load('assets/imgs/FOODCOMA.png').convert_alpha()  
         self.flip_meter_rect4 = self.flip_meter_surface4.get_rect(center = (4*self.screen_width/5, self.screen_height/10))
 
-        self.flip_meter = [self.flip_meter_surface1, self.flip_meter_surface2, self.flip_meter_surface3, self.flip_meter_surface4, self.flip_meter_rect1, self.flip_meter_rect2, self.flip_meter_rect3, self.flip_meter_rect4]
-
-
-
-    def check_match(self):
-        #i want to 
-        pass
+        self.flip_meter_surfaces = [self.flip_meter_surface1, self.flip_meter_surface2, self.flip_meter_surface3, self.flip_meter_surface4]
+        self.flip_meter_rects = [self.flip_meter_rect1, self.flip_meter_rect2, self.flip_meter_rect3, self.flip_meter_rect4]
 
     def set_flip_height(self):
-        flip_height = randint(1,4)
-        self.screen.blit(self.flip_meter[flip_height], self.flip_meter[flip_height*2])
+        self.flip_height = randint(1,4)
+        self.screen.blit(self.flip_meter[self.flip_height-1], self.flip_meter[self.flip_height*2])
+
+    def check_match(self, events):
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+
+                if event.key == pygame.key.key_code(f"{self.flip_height}"):
+                    return True
+
+
 
 
     def run(self):
-        
-        self.screen.fill("#DCD6F7")
-        events = pygame.events.get
-        #i need to go through 15 randomly generated intervals of pancake flipping heights, mapped to 1234
-        #i need to have 
-        for event in events:
-            if event.type == pygame.KEYDOWN:
-                if event.key == 1:
-                    pass
+        while self.running:
+            self.screen.fill("#DCD6F7")
+            events = pygame.event.get()
+
+            for i in range(1,16):
+                passed = False
+                while not passed:
+                    passed = self.check_match(events)
+
+            self.completed = True
+            self.running = False
+
+        if self.completed:
+            stacking = Stacking(self.screen, self.clock)
+            stacking.run()
+
+            
+                        
+
---
+++ stacking.py
@@ -0,0 +1,9 @@
+import pygame
+
+class Stacking:
+    def __init__(self, screen, clock):
+        self.screen = screen
+        self.clock = clock
+        self.running = True
+        self.completed = False
+
